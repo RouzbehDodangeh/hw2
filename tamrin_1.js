a23d46bfb0a3e103419703633a8826c0161681b2
RouzbehDodangeh <dodangehrouzbeh1@gmail.com>
2022-08-23 20:25:31 +0430

    hw2 added to the repositories

---
+++ tamrin_1.js
@@ -0,0 +1,69 @@
+
+//console.log( Number("a234A")); //or //console.log(+("1234A"))=> NaN; 
+//.............................
+//console.log('1'+2+2)=> 122;
+//.............................
+/*let apple="2";
+let orange="3";
+console.log(+apple + +orange); => 5;*/
+//..............................
+//let counter=1;
+//console.log(2* ++counter); => 4 
+//...............................
+//console.log(undefined==null); => true;
+//...............................
+//console.log(undefined===null);=> false
+//..................................
+//console.log(null===+"\n0\n");=> false
+//...................................
+//console.log(null=="\n0\n");=> false
+//...................................
+/*var number_1=3;
+var number_2=4;
+var number_3=5;
+if (number_1 + number_2>number_3 && number_2 + number_3>number_1 && number_1 + number_3>number_2) 
+    console.log("Yes!");
+    else
+    console.log("No!");*/
+//.............................................
+//...................
+/*var n=4567;
+counter=0;
+while (n > 1) {
+   
+    counter++;
+    n = n / 10;
+}
+console.log(counter);*/
+/*n=145;
+const count = n => String((Math.abs(n))).length;
+console.log(count(n));*/
+//.........................
+/*var number_1=234478;
+var count=0;
+for ( let index = 1;number_1>1; index++) {
+    number_1=number_1/10;
+    count=index;
+}
+console.log(count);
+*/
+//..............................
+
+/*var number_1=16;
+var number_2=8;
+var max= number_1 >number_2 ? max =number_1: maximum=number_2;
+for (let i = 0; i <= max/2; i++) {
+    if(number_1%i==0 && number_2% i==0)
+    console.log(i);
+}*/
+/*var n,m,i,q;
+n=91;
+m=32;
+for(i=1;i<=n;i++)
+    if(n%i==0){
+    for(q=1;q<=m;q++)
+    if(n%q==0 && i==q){
+        console.log(q);
+    }
+    }
+    */
